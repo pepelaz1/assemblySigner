66b731126896e4636b699849ec851ca76172ceae
Vladimir Baldetskiy <pepelaz@sibmail.com>
2014-11-11 22:44:56 +0600

    MRogerApp added

---
+++ MRogerApp/MRogerApp/output/MainForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace MRogerApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCallFn1 = new System.Windows.Forms.Button();
+            this.tbXml = new System.Windows.Forms.TextBox();
+            this.btnCallFn2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbAssemblies = new System.Windows.Forms.ListBox();
+            this.btnCallFn3 = new System.Windows.Forms.Button();
+            this.tbDir = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCallFn1
+            // 
+            this.btnCallFn1.Location = new System.Drawing.Point(12, 24);
+            this.btnCallFn1.Name = "btnCallFn1";
+            this.btnCallFn1.Size = new System.Drawing.Size(88, 23);
+            this.btnCallFn1.TabIndex = 0;
+            this.btnCallFn1.Text = "Call function 1";
+            this.btnCallFn1.UseVisualStyleBackColor = true;
+            this.btnCallFn1.Click += new System.EventHandler(this.btnCallFn1_Click);
+            // 
+            // tbXml
+            // 
+            this.tbXml.Location = new System.Drawing.Point(125, 44);
+            this.tbXml.Multiline = true;
+            this.tbXml.Name = "tbXml";
+            this.tbXml.Size = new System.Drawing.Size(593, 109);
+            this.tbXml.TabIndex = 1;
+            // 
+            // btnCallFn2
+            // 
+            this.btnCallFn2.Location = new System.Drawing.Point(12, 177);
+            this.btnCallFn2.Name = "btnCallFn2";
+            this.btnCallFn2.Size = new System.Drawing.Size(88, 23);
+            this.btnCallFn2.TabIndex = 2;
+            this.btnCallFn2.Text = "Call function 2";
+            this.btnCallFn2.UseVisualStyleBackColor = true;
+            this.btnCallFn2.Click += new System.EventHandler(this.btnCallFn2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Loaded assemblies";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Xml";
+            // 
+            // lbAssemblies
+            // 
+            this.lbAssemblies.FormattingEnabled = true;
+            this.lbAssemblies.Location = new System.Drawing.Point(129, 196);
+            this.lbAssemblies.Name = "lbAssemblies";
+            this.lbAssemblies.Size = new System.Drawing.Size(589, 121);
+            this.lbAssemblies.TabIndex = 6;
+            // 
+            // btnCallFn3
+            // 
+            this.btnCallFn3.Location = new System.Drawing.Point(12, 335);
+            this.btnCallFn3.Name = "btnCallFn3";
+            this.btnCallFn3.Size = new System.Drawing.Size(88, 23);
+            this.btnCallFn3.TabIndex = 7;
+            this.btnCallFn3.Text = "Call function 3";
+            this.btnCallFn3.UseVisualStyleBackColor = true;
+            this.btnCallFn3.Click += new System.EventHandler(this.btnCallFn3_Click);
+            // 
+            // tbDir
+            // 
+            this.tbDir.Location = new System.Drawing.Point(129, 355);
+            this.tbDir.Name = "tbDir";
+            this.tbDir.Size = new System.Drawing.Size(589, 20);
+            this.tbDir.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 335);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Directory extracted to";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 391);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbDir);
+            this.Controls.Add(this.btnCallFn3);
+            this.Controls.Add(this.lbAssemblies);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCallFn2);
+            this.Controls.Add(this.tbXml);
+            this.Controls.Add(this.btnCallFn1);
+            this.Name = "MainForm";
+            this.Text = "Main form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCallFn1;
+        private System.Windows.Forms.TextBox tbXml;
+        private System.Windows.Forms.Button btnCallFn2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbAssemblies;
+        private System.Windows.Forms.Button btnCallFn3;
+        private System.Windows.Forms.TextBox tbDir;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MRogerApp/MRogerApp/output/MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace MRogerApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private XmlDocument DecompressXmlFile(string compressed_xml_file)
+        {
+            XmlDocument xml = null;
+            try
+            {
+                xml = new XmlDocument();
+                using (FileStream fs = File.OpenRead(compressed_xml_file))
+                {
+                    using (GZipStream gz = new GZipStream(fs, CompressionMode.Decompress))
+                    {
+
+                        const int size = 4096;
+                        byte[] buffer = new byte[size];
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            int count = 0;
+                            do
+                            {
+                                count = gz.Read(buffer, 0, size);
+                                if (count > 0)
+                                {
+                                    ms.Write(buffer, 0, count);
+                                }
+                            }
+                            while (count > 0);
+                            ms.Position = 0;
+                            using (StreamReader sr = new StreamReader(ms))
+                                xml.LoadXml(sr.ReadToEnd());
+                        }                        
+                    }
+                }
+            }
+            finally
+            {
+
+            }
+            return xml;
+        }
+
+        
+        private void btnCallFn1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xdoc = DecompressXmlFile("note.xml.gz");
+            if (xdoc != null)
+                tbXml.Text = xdoc.OuterXml;
+        }
+
+
+        private void LoadAssembly(string compressed_assembly_resource)
+        {
+            try
+            {
+                using (Stream s = Assembly.GetEntryAssembly().GetManifestResourceStream(compressed_assembly_resource))
+                {
+                    using (GZipStream gz = new GZipStream(s, CompressionMode.Decompress))
+                    {
+                        const int size = 4096;
+                        byte[] buffer = new byte[size];
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            int count = 0;
+                            do
+                            {
+                                count = gz.Read(buffer, 0, size);
+                                if (count > 0)
+                                {
+                                    ms.Write(buffer, 0, count);
+                                }
+                            }
+                            while (count > 0);
+                            ms.Position = 0;
+                            Assembly.Load(ms.GetBuffer());
+                        }
+                    }
+                }
+
+            }
+            finally
+            {
+                
+            }
+        }
+
+        private string DecompressAssemblyInTempDir(string resource_name)
+        {
+            String result = "";
+            try
+            {
+                using (Stream s = Assembly.GetEntryAssembly().GetManifestResourceStream(resource_name))
+                {
+                    using (GZipStream gz = new GZipStream(s, CompressionMode.Decompress))
+                    {
+                        const int size = 4096;
+                        byte[] buffer = new byte[size];
+                        result = Path.GetTempPath();
+                        String[] parts = resource_name.Split(".".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                        String filename = result + parts[parts.Length-3] + "." + parts[parts.Length-2];
+                        using (FileStream ms = File.Create(filename))
+                        {
+                            int count = 0;
+                            do
+                            {
+                                count = gz.Read(buffer, 0, size);
+                                if (count > 0)
+                                {
+                                    ms.Write(buffer, 0, count);
+                                }
+                            }
+                            while (count > 0);
+                            ms.Close();
+                        }
+                    }
+                }
+
+            }
+            finally
+            {
+                
+            }
+            return result;
+        }
+
+        private void btnCallFn2_Click(object sender, EventArgs e)
+        {
+           LoadAssembly("MRogerApp.Resources.TestAssembly.dll.gz");
+           ShowLoadedAssemblies();  
+        }
+
+        private void ShowLoadedAssemblies()
+        {
+            lbAssemblies.Items.Clear();
+            AppDomain MyDomain = AppDomain.CurrentDomain;
+            Assembly[] AssembliesLoaded = MyDomain.GetAssemblies();
+            foreach (Assembly a in AssembliesLoaded)
+                lbAssemblies.Items.Add(a.FullName);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ShowLoadedAssemblies();  
+        }
+
+        private void btnCallFn3_Click(object sender, EventArgs e)
+        {
+            tbDir.Text = DecompressAssemblyInTempDir("MRogerApp.Resources.TestAssembly.dll.gz");
+        }
+    }
+}
